b03cfe6688139552850cdf48e612f50dd532159a
officialdev23 <121689836+officialdev23@users.noreply.github.com>
2023-01-14 21:46:01 +0530

    learned array in react

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import Card from './Card';
 import pic from './img/dark.jpg';
+import Sdata from "./Sdata";
+//import pic1 from  './img/flower.jpg'
+console.log(Sdata[0].sname);
 
 function App(){
     return(
         <> 
+        <h1 className="heading_style">Top React Series</h1>
     <Card imgsrc= {pic}
-    alt = 'myPic' 
-    title = "A Netflix Original Series"
-    sname = "Dark"
-    link = 'https://www.netflix.com/in/title/80100172' />
+    alt = {Sdata[0].alt} 
+    title = {Sdata[0].title}
+    sname = {Sdata[0].sname}
+    link = {Sdata[0].sname} />
 
-<Card imgsrc= {pic}
-    alt = 'myPic' 
-    title = "A Netflix Original Series"
-    sname = "Dark"
-    link = 'https://www.netflix.com/in/title/80100172' />
+    <Card imgsrc= {pic}
+    alt = {Sdata[1].alt} 
+    title = {Sdata[1].title}
+    sname = {Sdata[1].sname}
+    link = {Sdata[1].sname} />
 
-<Card imgsrc= {pic}
-    alt = 'myPic' 
-    title = "A Netflix Original Series"
-    sname = "Dark"
-    link = 'https://www.netflix.com/in/title/80100172' />
+    <Card imgsrc= {pic}
+    alt = {Sdata[2].alt} 
+    title = {Sdata[2].title}
+    sname = {Sdata[2].sname}
+    link = {Sdata[2].sname} />
   </>
 
     );
---
+++ netflix/src/Sdata.jsx
@@ -0,0 +1,26 @@
+import pic from './img/dark.jpg';
+
+const Sdata = [
+    {
+        imgscr: {pic},
+        alt : 'myPic', 
+        title : "A Netflix Original Series",
+        sname : "Dark",
+        link : 'https://www.netflix.com/in/title/80100172'
+    },
+    {
+        imgsrc: { pic },
+        alt : 'myPic', 
+        title : "A Netflix Original Series",
+        sname : "Dark",
+        link : 'https://www.netflix.com/in/title/80100172'
+    },
+    {
+        imgsrc: { pic },
+        alt : 'myPic', 
+        title : "A Netflix Original Series",
+        sname : "Dark",
+        link : 'https://www.netflix.com/in/title/80100172'
+    }
+]
+export default Sdata;
